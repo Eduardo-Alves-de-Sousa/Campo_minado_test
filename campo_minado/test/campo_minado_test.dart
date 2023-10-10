2292c566ce5b01d89dd520fdd3f2d50c3c820865
Eduardo-Alves-de-Sousa <eduardoalvessousa031@gmail.com>
2023-10-09 21:33:58 -0300

    Removendo: comentarios e linhas de codigos desnecessarios do arquivo teste

--- campo_minado/test/campo_minado_test.dart
+++ campo_minado/test/campo_minado_test.dart
@@ -22,15 +22,6 @@ void main() {
       // Verifica se o número de linhas e colunas é igual ao esperado para o nível fácil
       expect(rows, equals(8));
       expect(cols, equals(8));
-
-      // // Obtém o número esperado de linhas e colunas com base na dificuldade fácil
-      // int expectedRows = Difficulty.easy.boardRows;
-      // int expectedCols = Difficulty.easy.boardCols;
-
-      // // Verifica se o número de linhas é igual ao esperado
-      // expect(rows, equals(expectedRows));
-      // // Verifica se o número de colunas é igual ao esperado
-      // expect(cols, equals(expectedCols));
     });
 
     // Teste para iniciar o jogo no nível "Médio"
@@ -46,15 +37,6 @@ void main() {
       // Verifica se o número de linhas e colunas é igual ao esperado para o nível médio
       expect(rows, equals(10));
       expect(cols, equals(16));
-
-      // // Obtém o número esperado de linhas e colunas com base na dificuldade Médio
-      // int expectedRows = Difficulty.medium.boardRows;
-      // int expectedCols = Difficulty.medium.boardCols;
-
-      // // Verifica se o número de linhas é igual ao esperado
-      // expect(rows, equals(expectedRows));
-      // // Verifica se o número de colunas é igual ao esperado
-      // expect(cols, equals(expectedCols));
     });
 
     // Teste para iniciar o jogo no nível "Difícil"
@@ -69,12 +51,6 @@ void main() {
       // Verifica se o número de linhas e colunas é igual ao esperado para o nível dífício
       expect(rows, equals(24));
       expect(cols, equals(24));
-
-      // int expectedRows = Difficulty.hard.boardRows;
-      // int expectedCols = Difficulty.hard.boardCols;
-
-      // expect(rows, equals(expectedRows));
-      // expect(cols, equals(expectedCols));
     });
 
     // Teste para selecionar a dificuldade do jogo
@@ -96,24 +72,6 @@ void main() {
       g.setDifficulty(Difficulty.hard);
       expect(g.getBoard().length, equals(24));
       expect(g.getBoard()[0].length, equals(24));
-
-      // // Verifica se o tamanho do tabuleiro corresponde à dificuldade Fácil
-      // expect(g.getBoard().length, equals(Difficulty.easy.boardRows));
-      // expect(g.getBoard()[0].length, equals(Difficulty.easy.boardCols));
-
-      // // Define a dificuldade como médio
-      // g.setDifficulty(Difficulty.medium);
-
-      // // Verifica se o tamanho do tabuleiro corresponde à dificuldade média
-      // expect(g.getBoard().length, equals(Difficulty.medium.boardRows));
-      // expect(g.getBoard()[0].length, equals(Difficulty.medium.boardCols));
-
-      // // Define a dificuldade como difícil
-      // g.setDifficulty(Difficulty.hard);
-
-      // // Verifica se o tamanho do tabuleiro corresponde à dificuldade difícil
-      // expect(g.getBoard().length, equals(Difficulty.hard.boardRows));
-      // expect(g.getBoard()[0].length, equals(Difficulty.hard.boardCols));
     });
   });
 }
