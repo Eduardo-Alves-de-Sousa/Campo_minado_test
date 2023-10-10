18d996b0c2823dbdc25b6e15f1969b9b3588941a
Eduardo-Alves-de-Sousa <eduardoalvessousa031@gmail.com>
2023-10-09 21:29:59 -0300

    removendo erro da pasta bin arquivo campo_minado.dart

--- campo_minado/bin/campo_minado.dart
+++ campo_minado/bin/campo_minado.dart
@@ -1,5 +1,6 @@
+// ignore: unused_import
 import 'package:campo_minado/campo_minado.dart' as campo_minado;
 
 void main(List<String> arguments) {
-  print('Hello world: ${campo_minado.calculate()}!');
+  print('Hello world:!');
 }
