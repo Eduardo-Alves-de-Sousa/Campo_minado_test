cd5de9bd6520bd6ad37de31182644664ec80ef59
Eduardo-Alves-de-Sousa <eduardoalvessousa031@gmail.com>
2023-10-17 16:35:12 -0300

    Adicionado: Teste de descobrir zona sem bandeira e teste de Não é possível cobrir zona depois de descobri-la.

--- campo_minado/lib/jogador.dart
+++ campo_minado/lib/jogador.dart
@@ -12,13 +12,15 @@ class Jogador extends Game {
         linha < getBoard().length &&
         coluna >= 0 &&
         coluna < getBoard()[0].length) {
-      // Marca a zona com uma bandeira na posição (linha, coluna)
-      if (getBoard()[linha][coluna] != 'F') {
-        getBoard()[linha][coluna] =
-            'F'; // Supondo que 'F' representa uma bandeira
-      } else {
-        getBoard()[linha][coluna] =
-            'O'; // Remove a bandeira se já estiver marcada
+      // Verifica se a zona já foi descoberta
+      if (getBoard()[linha][coluna] != 'D') {
+        // Marca a zona com uma bandeira na posição (linha, coluna)
+        if (getBoard()[linha][coluna] != 'F') {
+          getBoard()[linha][coluna] = 'F'; // 'F' representa uma bandeira
+        } else {
+          getBoard()[linha][coluna] =
+              'O'; // Remove a bandeira se já estiver marcada
+        }
       }
     }
   }
@@ -29,8 +31,7 @@ class Jogador extends Game {
         coluna >= 0 &&
         coluna < getBoard()[0].length) {
       // Verifica se há uma bandeira na posição (linha, coluna)
-      return getBoard()[linha][coluna] ==
-          'F'; // Supondo que 'F' representa uma bandeira
+      return getBoard()[linha][coluna] == 'F'; // 'F' representa uma bandeira
     }
     return false;
   }
@@ -41,8 +42,7 @@ class Jogador extends Game {
         coluna >= 0 &&
         coluna < getBoard()[0].length) {
       // Remove a bandeira da zona na posição (linha, coluna)
-      getBoard()[linha][coluna] =
-          'O'; // Supondo que 'O' representa uma célula não revelada
+      getBoard()[linha][coluna] = 'O'; // 'O' representa uma célula não revelada
     }
   }
 
@@ -55,5 +55,29 @@ class Jogador extends Game {
       }
     }
     return true;
+  }
+
+  void descobrirZona(int linha, int coluna) {
+    if (linha >= 0 &&
+        linha < getBoard().length &&
+        coluna >= 0 &&
+        coluna < getBoard()[0].length) {
+      // Verifica se a zona não está marcada com bandeira
+      if (getBoard()[linha][coluna] != 'F') {
+        // Marca a zona como descoberta
+        getBoard()[linha][coluna] = 'D'; // 'D' representa zona descoberta
+      }
+    }
+  }
+
+  bool estaDescoberta(int linha, int coluna) {
+    if (linha >= 0 &&
+        linha < getBoard().length &&
+        coluna >= 0 &&
+        coluna < getBoard()[0].length) {
+      // Verifica se a zona está descoberta
+      return getBoard()[linha][coluna] == 'D'; // 'D' representa zona descoberta
+    }
+    return false;
   }
 }
--- campo_minado/test/jogador_test.dart
+++ campo_minado/test/jogador_test.dart
@@ -117,5 +117,45 @@ void main() {
       // Verificar se a bandeira foi removida
       expect(jogador.temBandeira(linha, coluna), isFalse);
     });
+    test('Descobrir Zona sem Bandeira', () {
+      Jogador jogador = Jogador();
+      jogador.iniciarJogo();
+
+      // Suponhamos que (linha, coluna) seja uma coordenada válida no tabuleiro.
+      int linha = 0;
+      int coluna = 0;
+
+      // Garanta que a zona não esteja marcada com bandeira
+      jogador.removerBandeira(linha, coluna);
+
+      // Descobrir a zona
+      jogador.descobrirZona(linha, coluna);
+
+      // Verificar se a zona está descoberta
+      expect(jogador.estaDescoberta(linha, coluna), isTrue);
+
+      // Verificar se o jogo ainda está em andamento (não foi perdido)
+      expect(jogador.isGameLost(), isFalse);
+
+      // Verificar se o tabuleiro foi atualizado corretamente (ou seja, sem bombas adjacentes)
+      expect(jogador.getBoard()[linha][coluna], isNot(' '));
+    });
+    test('Não é possível cobrir zona depois de descobri-la', () {
+      Jogador jogador = Jogador();
+      jogador.iniciarJogo();
+
+      // Suponhamos que (linha, coluna) seja uma coordenada válida no tabuleiro.
+      int linha = 0;
+      int coluna = 0;
+
+      // Descobrir a zona
+      jogador.descobrirZona(linha, coluna);
+
+      // Tente marcar a zona com uma bandeira depois de descobri-la
+      jogador.marcarComBandeira(linha, coluna);
+
+      // Verificar se a zona não está marcada com uma bandeira
+      expect(jogador.temBandeira(linha, coluna), isFalse);
+    });
   });
 }
